6be32a2dcc944d53f808b74db743e8d5a9eef50e
user.name  : "Abhi Choudhary <user.email : "abhiiichoudhary01@gmail.com>
2025-10-23 15:23:24 +0530

    Showcase component added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import ProductViewer from "./components/ProductViewer";
 import { ScrollTrigger } from "gsap/all";
 import { gsap } from "gsap";
+import Showcase from "./components/Showcase";
 
 
 gsap.registerPlugin(ScrollTrigger);
@@ -11,6 +12,8 @@ const App = () => {
     <Navbar />
     <Hero />
     <ProductViewer />
+    <Showcase />
+    <div className="h-screen bg-blue-400 overflow-hidden"></div>
   </div>;
 };
 
